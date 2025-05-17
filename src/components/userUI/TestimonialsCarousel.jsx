ce9b0e92dd54681d19b9231decd8ecaba754d7d3
VinnikA <vamposht@gmail.com>
2025-05-17 15:39:40 +0300

    testimonials section is ready

---
+++ src/components/userUI/TestimonialsCarousel.jsx
@@ -0,0 +1,199 @@
+'use client'
+
+import { useState } from "react"
+import TestimonialsCard from "./TestimonialsCard" 
+import LeftIcon from '../../../public/icons/chevron-left-32.svg'
+import RightIcon from '../../../public/icons/chevron-right-32.svg'
+
+const TestimonialsCarousel = () => {
+
+  const lang = 'uk'
+
+  const testimonials = [
+    {
+      uk: {
+        name: 'user name 1',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 1',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 2',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 2',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 3',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 3',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 4',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 4',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 5',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 5',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 6',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 6',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 7',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 7',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 8',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 8',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 9',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 9',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    },
+    {
+      uk: {
+        name: 'user name 10',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      },
+      en: {
+        name: 'user name 10',
+        imgUrl: '',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
+      }
+    }
+  ]
+
+  const [currentPage, setCurrentPage] = useState(0)
+  const itemsPerPage = 4 
+  const totalPages = Math.ceil(testimonials.length / itemsPerPage)
+
+  const currentTestimonials = testimonials.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  )
+
+  const handleNext = () => {
+    if (currentPage < totalPages - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  const handlePrev = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  return (
+    <div className="h-full flex flex-col py-5">
+
+      <div className="grow">
+        <div className='grid grid-cols-2 gap-10 mb-6'>
+          {currentTestimonials.map((item, index) => (
+            <div key={index} className="w-full flex justify-center">
+              <TestimonialsCard key={index} testimonial={item[lang]}/>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center gap-6 py-4 px-8">
+        <button
+          onClick={handlePrev}
+          disabled={currentPage === 0}
+          className={`w-8 h-8 rounded-full bg-bronze flex justify-center items-center ${
+            currentPage === 0 ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+        >
+          <LeftIcon className='text-white'/>
+        </button>
+
+        <span className="text-gray-700">
+          Наступні
+        </span>
+
+        <button
+          onClick={handleNext}
+          disabled={currentPage === totalPages - 1}
+          className={`w-8 h-8 rounded-full bg-bronze flex justify-center items-center ${
+            currentPage === totalPages - 1 ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+        >
+          <RightIcon className='text-white'/>
+        </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default TestimonialsCarousel
--- src/components/userUI/TestimonialsSection.jsx
+++ src/components/userUI/TestimonialsSection.jsx
@@ -1,88 +1,22 @@
 import BabyImg from '../../../public/images/baby.svg'
-import TestimonialsCard from './TestimonialsCard'
+import TestimonialsCarousel from './TestimonialsCarousel'
 
 const TestimonialsSection = () => {
 
   const lang = 'uk' 
 
-  const testimonials = [
-    {
-      uk: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      },
-      en: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      }
-    },
-    {
-      uk: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      },
-      en: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      }
-    },
-    {
-      uk: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      },
-      en: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      }
-    },
-    {
-      uk: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      },
-      en: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      }
-    },
-    {
-      uk: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      },
-      en: {
-        name: 'user name',
-        imgUrl: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, officiis.'
-      }
-    }
-  ]
-
   return (
-    <section className='grid grid-cols-2 py-14 gap-10'>
+    <section className='grid grid-cols-2 py-14 gap-10 min-h-[720px]'>
       <div className='flex items-center justify-center'>
         <BabyImg className='max-w-[600px] h-auto' />
       </div>
-      <div>
-        <h3></h3>
-        <div className='grid grid-cols-2 gap-10'>
-          {testimonials.map((item, index) => (
-            <TestimonialsCard key={index} testimonial={item[lang]}/>
-          ))}
+      <div className='flex flex-col'>
+        <h3 className='text-[40px] font-bold mb-6'>Відгуки про нас</h3>
+        <p className='mb-6'>Що пишуть про нашу роботу</p>
+        <div className='grow'>
+          <TestimonialsCarousel />
         </div>
       </div>
-
-
     </section>
   )
 }
