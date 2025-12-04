fa4540eaa00311f040c670befa5c3b3dea4c6784
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-12-04 20:21:46 +0200

    upd base URL & locale product page

--- src/app/[locale]/(public)/product/page.tsx
+++ src/app/[locale]/(public)/product/page.tsx
@@ -1,7 +1,15 @@
 // src/app/[locale]/(public)/product/page.tsx
+import { Locale } from '@/types/baseTypes'
+
 import { redirect } from 'next/navigation'
 
-export default function ProductRootPage({ params }: { params: { locale: string } }) {
-	redirect(`/${params.locale}/catalog`)
+export default async function ProductRootPage({
+	params
+}: {
+	params: Promise<{ slug: string; locale: Locale }>
+}) {
+	const { locale } = await params
+
+	redirect(`/${locale}/catalog`)
 }
 // сторінка у даний момент не потрібна. робимо автоматичний редирект на каталог
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
-export const BASE_URL = 'http://localhost:5000/api'
-export const BASE_IMG_URL = 'http://localhost:5000'
+// export const BASE_URL = 'http://localhost:5000/api'
+// export const BASE_IMG_URL = 'http://localhost:5000'
 
-// export const BASE_URL = 'https://devtestapps.online/api'
+export const BASE_URL = 'https://devtestapps.online/api'
+export const BASE_IMG_URL = 'https://devtestapps.online'
+
 // export const BASE_URL = 'http://31.42.190.66:5000/api'
-
-export const WHOLESALE_DISCOUNT = 0.07 // знижка для опту. з голови взято 7% для відладки сайту
