2a89a52fe2539dca59e49d3130efb368a9e2d93e
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2026-01-04 21:56:21 +0200

    upd newsPage

--- src/app/[locale]/(public)/news/[slug]/page.tsx
+++ src/app/[locale]/(public)/news/[slug]/page.tsx
@@ -27,8 +27,8 @@ export async function generateMetadata({ params: { slug, locale } }: { params: P
 	}
 }
 
-const OneNews = async ({ params }: { params: Promise<Params> }) => {
-	const { slug, locale } = await params
+const OneNews = async ({ params }: { params: Params }) => {
+	const { slug, locale } = params
 
 	const t = await getTranslations({ locale })
 	const titles = [
