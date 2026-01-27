e59aa84c21e74f0700ea92478330905b60aadc2e
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2026-01-27 23:56:44 +0200

    fetch admin categories

--- src/app/[locale]/(admin)/manage-panel/products/page.tsx
+++ src/app/[locale]/(admin)/manage-panel/products/page.tsx
@@ -1,17 +1,21 @@
 import CategoriesAdminContainer from '@/components/adminUI/CategoriesAdminContainer'
 import HeaderPage from '@/components/adminUI/HeaderPage'
 
+import { fetchPublicCategories } from '@/api/api-fetch/categories'
+
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
 	title: 'Всі продукти | RW-Trade'
 }
-const Cards = () => {
+const Cards = async () => {
+	const categories = await fetchPublicCategories()
+
 	return (
 		<div className='w-full relative'>
 			<HeaderPage pageName='Всі продукти' />
 			{/* <CategoriesControlAdminPage /> */}
-			<CategoriesAdminContainer locale='uk' />
+			<CategoriesAdminContainer locale='uk' initialCategories={categories} />
 		</div>
 	)
 }
